0535d1b8f03b04d38093d2fe13e5e88cfee9879e
Dan Brough <dan@danbrough.org>
2021-10-21 17:06:07 +1300

    v0.0.1-alpha42

--- cmd/gomobile/bind_linux.go
+++ cmd/gomobile/bind_linux.go
@@ -35,8 +35,8 @@ func goLinuxBind(gobind string, pkgs []*packages.Package, targets []targetInfo) 
 			println("FOUND ", s)
 			javaHome = &strings.Split(s, "=")[1]
 			println("FOUND JAVA_HOME:", *javaHome)
-			cmd.Env = append(cmd.Env, "CGO_CFLAGS=-I"+*javaHome+"/include -I"+*javaHome+"/include/linux")
-			break
+			//cmd.Env = append(cmd.Env, "CGO_CFLAGS=-I"+*javaHome+"/include -I"+*javaHome+"/include/linux")
+			//cmd.Env = append(cmd.Env, "CFLAGS=-I"+*javaHome+"/include -I"+*javaHome+"/include/linux")
 		} else if strings.HasPrefix(s, "CFLAGS") {
 			cmd.Env = append(cmd.Env, s)
 		}
