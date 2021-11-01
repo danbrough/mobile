11926c097b6dbab433e210f9e4d874f604db3fcf
Dan Brough <dan@danbrough.org>
2021-11-01 13:22:57 +1300

    update

---
+++ demos/basic/android.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+
+[ ! -d libs ] && mkdir libs
+
+go run github.com/danbrough/mobile/cmd/gomobile bind -target=android/386 -x -v -work -o libs/demo.aar \
+  github.com/danbrough/mobile/demos/basic/hello || exit 1
+
+
