a5e2b1a35e47011904a22ebba61d2bdd87bcce64
Dan Brough <dan@danbrough.org>
2021-11-08 06:17:45 +1300

    update

--- demos/basic/Main.java
+++ demos/basic/Main.java
@@ -3,6 +3,7 @@ import hello.Hello;
 
 public class Main{
 	public static void main(String[] args){
+		System.loadLibrary("gojni");
 		System.out.println("Hello.getMessage() = " + Hello.getMessage());
 	}
 }
