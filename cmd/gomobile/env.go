c321d0bf2538a9a255fe698a449e4fcaaa7dd317
Dan Brough <dan@danbrough.org>
2021-11-18 15:52:13 +1300

    v0.0.1

--- cmd/gomobile/env.go
+++ cmd/gomobile/env.go
@@ -222,7 +222,7 @@ func envInit() (err error) {
 			}
 
 			if useOpenssl && sslLibsDir != "" {
-				sslDir := filepath.Join(sslLibsDir, arch)
+				sslDir := filepath.Join(sslLibsDir, toolchain.abi)
 				klog.KLog.Warn("Using static openssl libs at: %s", sslDir)
 				androidEnv[arch] = append(androidEnv[arch], "CGO_CFLAGS=-I"+filepath.Join(sslDir, "include"), "CGO_LDFLAGS=-L"+filepath.Join(sslDir,"lib"))
 			}
