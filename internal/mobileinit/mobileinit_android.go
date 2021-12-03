bddebd82300c5fcda7920b5b620771494c85bc1a
Dan Brough <dan@danbrough.org>
2021-12-04 09:51:22 +1300

    v0.0.2-beta04

--- internal/mobileinit/mobileinit_android.go
+++ internal/mobileinit/mobileinit_android.go
@@ -1,7 +1,9 @@
+//go:build linux || android
+// +build linux android
+
 // Copyright 2014 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
 package mobileinit
 
 /*
